834a3dcb17e9c356fe216d38c62e2cd27f73bc6b
MichelleCordovi <michellecordovi@gmail.com>
2024-08-19 14:21:36 -0400

    pull json data into app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css'
+import {useState, useEffect} from 'react'
+import data from '../data.json'
 
 function App() {
+  const [desserts] = useState(data)
 
+  useEffect(() => {
+    console.log(desserts)
+  }, [desserts])
+
+  return <p>HELLO</p>
 }
 
 export default App
