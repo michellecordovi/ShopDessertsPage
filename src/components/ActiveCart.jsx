da1ffc027e2f09f06c1e2a08effe8a406a47a7f2
MichelleCordovi <michellecordovi@gmail.com>
2024-08-20 14:17:50 -0400

    add styling to cart

--- src/components/ActiveCart.jsx
+++ src/components/ActiveCart.jsx
@@ -2,6 +2,8 @@
 import CartItemsGrid from "./CartItemsGrid";
 
 function ActiveCart({desserts, cartItems}){
+
+    ///calculates the total value for your order based on cartItems array
     function calculateTotal(){
         const priceArray = cartItems.map(index => {
             return desserts[index].price;
@@ -19,12 +21,12 @@ function ActiveCart({desserts, cartItems}){
 
             <div id="order-total">
                 <p>Order Total</p>
-                <h2>{calculateTotal()}</h2>
+                <h2>${calculateTotal()}</h2>
             </div>
 
             <div id="carbon-neutral-label">
                 <img src="./assets/images/icon-carbon-neutral.svg" alt="This is a carbon-neutral delivery" />
-                <p id="carbon-neutral-statement">This is a carbon-neutral delivery</p>
+                <p id="carbon-neutral-statement">This is a <span>carbon-neutral</span> delivery</p>
             </div>
 
             <button id="confirm-order-button">Confirm Order</button>
