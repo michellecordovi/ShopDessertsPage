71f4dda530bd5d463d870d87f063f5c8669cd569
MichelleCordovi <michellecordovi@gmail.com>
2024-08-20 09:17:17 -0400

    import json data to create desserts grid

--- src/components/DessertCard.jsx
+++ src/components/DessertCard.jsx
@@ -1,5 +1,18 @@
-function DessertCard() {
-    
+/* eslint-disable react/prop-types */
+function DessertCard({src, name, category, price}) {
+    return (
+        <div className="dessert-card">
+            <div className="dessert-image-container">
+                <img src={src} alt={name} />
+            </div>
+
+            <div className="dessert-description">
+                <p>{category}</p>
+                <h3>{name}</h3>
+                <p>{price}</p>
+            </div>
+        </div>
+    )
 }
 
 export default DessertCard;
--- src/components/DessertGrid.jsx
+++ src/components/DessertGrid.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable react/prop-types */
 import DessertCard from "./DessertCard";
 
 function DessertGrid({desserts}) {
     return (
-        desserts.map(dessert => {
-            <DessertCard src={dessert.image.desktop} name={dessert.name} category={dessert.category} price={dessert.price} />
-        })
+        <main>
+            <h1>Desserts</h1>
+
+            <section id="desserts-grid">
+                {desserts.map((dessert) => {
+                    return <DessertCard key={dessert.name} src={dessert.image.desktop} name={dessert.name} category={dessert.category} price={dessert.price} />
+                })}
+            </section>
+        </main>
     )
 }
 
