c3c2604c90b71b8efad79cd0eedc4216c7bfdb57
MichelleCordovi <michellecordovi@gmail.com>
2024-08-22 16:04:34 -0400

    create styling and hover effects for cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,7 +5,7 @@ import ActiveCart from "./ActiveCart";
 function Cart({desserts, cartItems, setCartItems, uniqueItems, setModalIsVisible, total}){
     return (
         <aside id="cart">
-            <h2>Your Cart({cartItems.length})</h2>
+            <h2>Your Cart ({cartItems.length})</h2>
             {cartItems.length === 0 ? <EmptyCartImage/> : <ActiveCart desserts={desserts} cartItems={cartItems} setCartItems={setCartItems} uniqueItems = {uniqueItems} setModalIsVisible={setModalIsVisible} total={total}/>}
         </aside>
     )
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -11,13 +11,13 @@ function CartItem({ desserts, price, name, count, cartItems, setCartItems}) {
         <div className="cart-item">
             <div className="cart-item-info">
                 <p>{name}</p>
-                <span>{count}x</span>
+                <span className="cart-item-count">{count}x</span>
                 <span className="cart-item-price">@ {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price)}</span>
                 <span className="cart-item-total">{new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(count * price)}</span>
             </div>
 
             <button className="remove-cart-item-button" onClick={handleClick}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10">
+                <svg xmlns="http://www.w3.org/2000/svg" width="10px" height="10px" fill="none" viewBox="0 0 10 10">
                     <path fill="#CAAFA7" d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"/>
                 </svg>
             </button>
