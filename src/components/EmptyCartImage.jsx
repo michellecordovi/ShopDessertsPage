1d76ccbbd2aa39bf148f6ae65e0372c76099c6a5
MichelleCordovi <michellecordovi@gmail.com>
2024-09-03 14:39:42 -0400

    add alt text to empty cart image

--- src/components/EmptyCartImage.jsx
+++ src/components/EmptyCartImage.jsx
@@ -1,7 +1,7 @@
 function EmptyCartImage(){
     return (
         <div id="empty-cart-image-container">
-            <img src="/assets/images/illustration-empty-cart.svg" alt="" />
+            <img src="/assets/images/illustration-empty-cart.svg" alt="empty cart image display, a cake with a slice cut out" />
             <p>Your added items will appear here</p>
         </div>
     )
