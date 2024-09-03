c2d8b6910772c4002a0dc434a798f8abfbb9189a
MichelleCordovi <michellecordovi@gmail.com>
2024-09-03 12:41:49 -0400

    adjust source of dessert grid image to be based on viewport width so that it adjust with desktop and mobile screens

--- src/components/DessertGrid.jsx
+++ src/components/DessertGrid.jsx
@@ -6,13 +6,13 @@ function DessertGrid({desserts, cartItems, setCartItems}) {
         <main>
             <h1>Desserts</h1>
 
-            <section id="desserts-grid">
-                {desserts.map((dessert, index) => {
-                    return <DessertCard key={index} index = {index} src={dessert.image.desktop} name={dessert.name} category={dessert.category} price={dessert.price} desserts={desserts} cartItems={cartItems} setCartItems={setCartItems} />
+      <section id="desserts-grid">
+        {desserts.map((dessert, index) => {
+                    return <DessertCard key={index} index = {index} src={window.innerWidth > 1000 ? dessert.image.desktop : dessert.image.mobile} name={dessert.name} category={dessert.category} price={dessert.price} desserts={desserts} cartItems={cartItems} setCartItems={setCartItems} />
                 })}
             </section>
         </main>
-    )
+  );
 }
 
 export default DessertGrid;
